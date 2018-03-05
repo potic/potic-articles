3879323fe10663686c3c9aac24943373f5db8f96
yermilov <yermilov17@gmail.com>
2018-03-06 01:24:01 +0200

    fix compilation errors

--- src/main/groovy/me/potic/articles/service/FeedbackService.groovy
+++ src/main/groovy/me/potic/articles/service/FeedbackService.groovy
@@ -1,17 +1,11 @@
 package me.potic.articles.service
 
 import groovy.util.logging.Slf4j
-import groovyx.net.http.HttpBuilder
 import me.potic.articles.domain.Article
 import me.potic.articles.domain.ArticleEvent
 import me.potic.articles.domain.ArticleEventType
 import me.potic.articles.domain.User
-import me.potic.sections.domain.Article
-import me.potic.sections.domain.ArticleEvent
-import me.potic.sections.domain.ArticleEventType
-import me.potic.sections.domain.User
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 import java.time.LocalDateTime
