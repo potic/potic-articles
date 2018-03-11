7aed1a7b85581979fd611d68cb9a2c01a178b703
yermilov <yermilov17@gmail.com>
2018-03-12 00:32:50 +0200

    #26. fix failing tests

--- src/integration-test/groovy/me/potic/articles/ArticlesServiceIntegrationTest.groovy
+++ src/integration-test/groovy/me/potic/articles/ArticlesServiceIntegrationTest.groovy
@@ -42,20 +42,20 @@ class ArticlesServiceIntegrationTest extends Specification {
         articlesService.mongoTemplate = mongoTemplate
 
         List articles = [
-                Article.builder().id('TEST_ARTICLE_1').userId('TEST_USER_1').card(new Card(actual: true, pocketId: 'POCKET_1')).fromPocket(new PocketArticle(item_id: 'POCKET_1', resolved_title: 'TITLE_1', status: '0', word_count: 100, time_added: 1)).build(),
-                Article.builder().id('TEST_ARTICLE_2').userId('TEST_USER_1').card(new Card(actual: true, pocketId: 'POCKET_2')).fromPocket(new PocketArticle(item_id: 'POCKET_2', resolved_title: 'TITLE_2', status: '0', word_count: 200, time_added: 2)).build(),
-                Article.builder().id('TEST_ARTICLE_3').userId('TEST_USER_1').card(new Card(actual: true, pocketId: 'POCKET_3')).fromPocket(new PocketArticle(item_id: 'POCKET_3', resolved_title: 'TITLE_3', status: '0', word_count: 300, time_added: 3)).build(),
-                Article.builder().id('TEST_ARTICLE_4').userId('TEST_USER_1').card(new Card(actual: true, pocketId: 'POCKET_4')).fromPocket(new PocketArticle(item_id: 'POCKET_4', resolved_title: 'TITLE_4', status: '1', word_count: 100, time_added: 4)).build(),
-                Article.builder().id('TEST_ARTICLE_5').userId('TEST_USER_2').card(new Card(actual: true, pocketId: 'POCKET_5')).fromPocket(new PocketArticle(item_id: 'POCKET_5', resolved_title: 'TITLE_5', status: '0', word_count: 200, time_added: 5)).build(),
-                Article.builder().id('TEST_ARTICLE_6').userId('TEST_USER_2').card(new Card(actual: true, pocketId: 'POCKET_6')).fromPocket(new PocketArticle(item_id: 'POCKET_6', resolved_title: 'TITLE_6', status: '1', word_count: 100, time_added: 6)).build(),
+                Article.builder().id('TEST_ARTICLE_1').userId('TEST_USER_1').card(new Card(timestamp: 2000, pocketId: 'POCKET_1', url: 'URL_1', title: 'TITLE_1')).fromPocket(new PocketArticle(item_id: 'POCKET_1', resolved_title: 'TITLE_1', status: '0', word_count: 100, time_added: 1)).build(),
+                Article.builder().id('TEST_ARTICLE_2').userId('TEST_USER_1').card(new Card(timestamp: 2000, pocketId: 'POCKET_2', url: 'URL_2', title: 'TITLE_2')).fromPocket(new PocketArticle(item_id: 'POCKET_2', resolved_title: 'TITLE_2', status: '0', word_count: 200, time_added: 2)).build(),
+                Article.builder().id('TEST_ARTICLE_3').userId('TEST_USER_1').card(new Card(timestamp: 2000, pocketId: 'POCKET_3', url: 'URL_3', title: 'TITLE_3')).fromPocket(new PocketArticle(item_id: 'POCKET_3', resolved_title: 'TITLE_3', status: '0', word_count: 300, time_added: 3)).build(),
+                Article.builder().id('TEST_ARTICLE_4').userId('TEST_USER_1').card(new Card(timestamp: 2000, pocketId: 'POCKET_4', url: 'URL_4', title: 'TITLE_4')).fromPocket(new PocketArticle(item_id: 'POCKET_4', resolved_title: 'TITLE_4', status: '1', word_count: 100, time_added: 4)).build(),
+                Article.builder().id('TEST_ARTICLE_5').userId('TEST_USER_2').card(new Card(timestamp: 2000, pocketId: 'POCKET_5', url: 'URL_5', title: 'TITLE_5')).fromPocket(new PocketArticle(item_id: 'POCKET_5', resolved_title: 'TITLE_5', status: '0', word_count: 200, time_added: 5)).build(),
+                Article.builder().id('TEST_ARTICLE_6').userId('TEST_USER_2').card(new Card(timestamp: 2000, pocketId: 'POCKET_6', url: 'URL_6', title: 'TITLE_6')).fromPocket(new PocketArticle(item_id: 'POCKET_6', resolved_title: 'TITLE_6', status: '1', word_count: 100, time_added: 6)).build(),
 
                 // same articles but non-actual
-                Article.builder().id('TEST_ARTICLE_01').userId('TEST_USER_1').card(new Card(actual: false, pocketId: 'POCKET_1')).fromPocket(new PocketArticle(item_id: 'POCKET_1', status: '0', word_count: 100, time_added: 1)).build(),
-                Article.builder().id('TEST_ARTICLE_02').userId('TEST_USER_1').card(new Card(actual: false, pocketId: 'POCKET_2')).fromPocket(new PocketArticle(item_id: 'POCKET_2', status: '0', word_count: 200, time_added: 2)).build(),
-                Article.builder().id('TEST_ARTICLE_03').userId('TEST_USER_1').card(new Card(actual: false, pocketId: 'POCKET_3')).fromPocket(new PocketArticle(item_id: 'POCKET_3', status: '0', word_count: 300, time_added: 3)).build(),
-                Article.builder().id('TEST_ARTICLE_04').userId('TEST_USER_1').card(new Card(actual: false, pocketId: 'POCKET_4')).fromPocket(new PocketArticle(item_id: 'POCKET_4', status: '1', word_count: 100, time_added: 4)).build(),
-                Article.builder().id('TEST_ARTICLE_05').userId('TEST_USER_2').card(new Card(actual: false, pocketId: 'POCKET_5')).fromPocket(new PocketArticle(item_id: 'POCKET_5', status: '0', word_count: 200, time_added: 5)).build(),
-                Article.builder().id('TEST_ARTICLE_06').userId('TEST_USER_2').card(new Card(actual: false, pocketId: 'POCKET_6')).fromPocket(new PocketArticle(item_id: 'POCKET_6', status: '1', word_count: 100, time_added: 6)).build()
+                Article.builder().id('TEST_ARTICLE_01').userId('TEST_USER_1').card(new Card(pocketId: 'POCKET_1', url: 'URL_1')).fromPocket(new PocketArticle(item_id: 'POCKET_1', status: '0', word_count: 100, time_added: 1)).build(),
+                Article.builder().id('TEST_ARTICLE_02').userId('TEST_USER_1').card(new Card(pocketId: 'POCKET_2', title: 'TITLE_2')).fromPocket(new PocketArticle(item_id: 'POCKET_2', status: '0', word_count: 200, time_added: 2)).build(),
+                Article.builder().id('TEST_ARTICLE_03').userId('TEST_USER_1').card(new Card(pocketId: 'POCKET_3')).fromPocket(new PocketArticle(item_id: 'POCKET_3', status: '0', word_count: 300, time_added: 3)).build(),
+                Article.builder().id('TEST_ARTICLE_04').userId('TEST_USER_1').card(new Card(pocketId: 'POCKET_4')).fromPocket(new PocketArticle(item_id: 'POCKET_4', status: '1', word_count: 100, time_added: 4)).build(),
+                Article.builder().id('TEST_ARTICLE_05').userId('TEST_USER_2').card(new Card(pocketId: 'POCKET_5')).fromPocket(new PocketArticle(item_id: 'POCKET_5', status: '0', word_count: 200, time_added: 5)).build(),
+                Article.builder().id('TEST_ARTICLE_06').userId('TEST_USER_2').card(new Card(pocketId: 'POCKET_6')).fromPocket(new PocketArticle(item_id: 'POCKET_6', status: '1', word_count: 100, time_added: 6)).build()
         ]
         mongoTemplate.insert(articles, Article)
     }
@@ -70,7 +70,7 @@ class ArticlesServiceIntegrationTest extends Specification {
         with(articles[0]) {
             id == 'TEST_ARTICLE_3'
             userId == 'TEST_USER_1'
-            card.actual == true
+            card.pocketId == 'POCKET_3'
             fromPocket.status == '0'
             fromPocket.word_count == 300
             fromPocket.time_added == 3
@@ -79,7 +79,7 @@ class ArticlesServiceIntegrationTest extends Specification {
         with(articles[1]) {
             id == 'TEST_ARTICLE_2'
             userId == 'TEST_USER_1'
-            card.actual == true
+            card.pocketId == 'POCKET_2'
             fromPocket.status == '0'
             fromPocket.word_count == 200
             fromPocket.time_added == 2
@@ -88,7 +88,7 @@ class ArticlesServiceIntegrationTest extends Specification {
         with(articles[2]) {
             id == 'TEST_ARTICLE_1'
             userId == 'TEST_USER_1'
-            card.actual == true
+            card.pocketId == 'POCKET_1'
             fromPocket.status == '0'
             fromPocket.word_count == 100
             fromPocket.time_added == 1
@@ -105,7 +105,7 @@ class ArticlesServiceIntegrationTest extends Specification {
         with(articles[0]) {
             id == 'TEST_ARTICLE_3'
             userId == 'TEST_USER_1'
-            card.actual == true
+            card.pocketId == 'POCKET_3'
             fromPocket.status == '0'
             fromPocket.word_count == 300
             fromPocket.time_added == 3
@@ -122,7 +122,7 @@ class ArticlesServiceIntegrationTest extends Specification {
         with(articles[0]) {
             id == 'TEST_ARTICLE_1'
             userId == 'TEST_USER_1'
-            card.actual == true
+            card.pocketId == 'POCKET_1'
             fromPocket.status == '0'
             fromPocket.word_count == 100
             fromPocket.time_added == 1
@@ -139,7 +139,7 @@ class ArticlesServiceIntegrationTest extends Specification {
         with(articles[0]) {
             id == 'TEST_ARTICLE_3'
             userId == 'TEST_USER_1'
-            card.actual == true
+            card.pocketId == 'POCKET_3'
             fromPocket.status == '0'
             fromPocket.word_count == 300
             fromPocket.time_added == 3
@@ -156,7 +156,7 @@ class ArticlesServiceIntegrationTest extends Specification {
         with(articles[0]) {
             id == 'TEST_ARTICLE_1'
             userId == 'TEST_USER_1'
-            card.actual == true
+            card.pocketId == 'POCKET_1'
             fromPocket.status == '0'
             fromPocket.word_count == 100
             fromPocket.time_added == 1
@@ -173,7 +173,7 @@ class ArticlesServiceIntegrationTest extends Specification {
         with(articles[0]) {
             id == 'TEST_ARTICLE_2'
             userId == 'TEST_USER_1'
-            card.actual == true
+            card.pocketId == 'POCKET_2'
             fromPocket.status == '0'
             fromPocket.word_count == 200
             fromPocket.time_added == 2
@@ -269,8 +269,6 @@ class ArticlesServiceIntegrationTest extends Specification {
         with(actual) {
             id != null
             userId == 'TEST_USER_1'
-            card.actual == false
-            card.id == id
             fromPocket.item_id == 'INGESTED_1'
             fromPocket.resolved_id == 'INGESTED_1'
             fromPocket.given_url == 'URL_1'
@@ -308,8 +306,6 @@ class ArticlesServiceIntegrationTest extends Specification {
         with(actual) {
             id == 'TEST_ARTICLE_1'
             userId == 'TEST_USER_1'
-            card.actual == false
-            card.id == 'TEST_ARTICLE_1'
             fromPocket.item_id == 'ALREADY_INGESTED_1'
             fromPocket.resolved_id == 'ALREADY_INGESTED_1'
             fromPocket.given_url == 'URL_1'
@@ -334,8 +330,6 @@ class ArticlesServiceIntegrationTest extends Specification {
         with(actual) {
             id != null
             userId == 'TEST_USER_1'
-            card.actual == false
-            card.id == id
             fromPocket.item_id == 'INGESTED_1'
         }
     }
